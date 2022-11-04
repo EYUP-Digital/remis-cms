93325c57501770913277f5662a9eed4f2cb338e4
Craig Boldy <info@craigb.dev>
2022-11-04 07:36:04 +0000

    feat: content tweaks

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,27 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      breakpoints: {
+        xxlarge: 1920,
+        xlarge: 1400,
+        large: 1000,
+        small: 500,
+      },
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
