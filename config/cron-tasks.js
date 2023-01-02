27336751b0b6e302ceb271c25b96e1633197a4ea
Craig Boldy <info@craigb.dev>
2023-01-02 19:28:35 +0000

    chore remove cron

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,27 +1,27 @@
-module.exports = {
-  '*/1 * * * *': {
-    task: async () => {
+// module.exports = {
+//   '*/1 * * * *': {
+//     task: async () => {
 
-      // fetch articles to publish
-      const draftArticleToPublish = await strapi.entityService.findMany('api::insight.insight', {
-        filters: {
-          publishedAt: { $null: true },
-          publish_at: { $lt: new Date() },
-        },
-        publicationState: 'preview'
-      });
+//       // fetch articles to publish
+//       const draftArticleToPublish = await strapi.entityService.findMany('api::insight.insight', {
+//         filters: {
+//           publishedAt: { $null: true },
+//           publish_at: { $lt: new Date() },
+//         },
+//         publicationState: 'preview'
+//       });
 
-      // update published_at of articles
-      await Promise.all(draftArticleToPublish.map(insight => {
-        return strapi.entityService.update('api::insight.insight', insight.id, {
-          data: {
-            publishedAt: new Date(),
-          },
-        });
-      }));
-    },
-    options: {
-      tz: 'Europe/London',
-    }
-  }
-};
+//       // update published_at of articles
+//       await Promise.all(draftArticleToPublish.map(insight => {
+//         return strapi.entityService.update('api::insight.insight', insight.id, {
+//           data: {
+//             publishedAt: new Date(),
+//           },
+//         });
+//       }));
+//     },
+//     options: {
+//       tz: 'Europe/London',
+//     }
+//   }
+// };
